922569e1e639e78de068bdd0668c4766aad0e570
ShashwatMishra0099 <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-04-04 15:23:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,19 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-TOKEN = "7049444312:AAEr1d-KXTz0mVR6QOPzRy-Nwa5NVk9o4rA"
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello! Type /fetchmembers to fetch members.')
 
 def fetch_members(update: Update, context: CallbackContext) -> None:
     try:
-        chat_id = update.message.chat_id
-        members = context.bot.get_chat_members(chat_id)  # Retrieve all chat members
-        
-        member_usernames = [member.user.username for member in members if member.user.username is not None]  # Extract usernames of members
-        members_count = len(member_usernames)  # Get the count of members with usernames
-        
+        chat_id = -1002010714711
+        members_count = context.bot.get_chat_member_count(chat_id)
         update.message.reply_text(f'The number of members in the chat: {members_count}')
-        update.message.reply_text('Usernames of members:')
-        update.message.reply_text('\n'.join(member_usernames))  # Display the usernames in a list
     except Exception as e:
         update.message.reply_text('An error occurred while fetching members. Please try again later.')
+
 def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
